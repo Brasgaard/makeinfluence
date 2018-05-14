5894f5691b5b2d900afc082caa21e802efd9cf43
Brasgaard <alexanderbrasgaard@hotmail.com>
2018-05-14 12:18:38 +0200

    Map structure
    - All the maps we need
    - Empty index, css and js file

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Title -->
+	<title>Title here</title>
+	
+	<!-- Description -->
+	<meta charset="UTF-8">
+	<meta name="Description" content="Insert your description here">
+
+	<!-- Keywords -->
+	<meta name="keywords" content="HTML,CSS,JavaScript">
+
+	<!-- Viewport -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Css, Sass and fonts-->
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <!-- Favicons -->
+	<link rel="icon" type="image/png" href="#" sizes="32x32" />
+	<link rel="icon" type="image/png" href="#" sizes="16x16" />
+</head>
+<body>
+
+
+
+
+<!-- Scripts -->
+<script src="#"></script>
+</body>
+</html>
