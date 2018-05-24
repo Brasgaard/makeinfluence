e624c14fccfa5c608cd212b5d25766966352287f
LukasBrondel <lukas_broendel@hotmail.com>
2018-05-24 17:21:16 +0200

    Most of index and header complete
    Intro, header, video, key features, animate.css, fontawesome and some
    image changes.

---
+++ include/header.php
@@ -0,0 +1,27 @@
+<header>
+    <img src="images/makeinfluence-logo-black.png" alt="MakeInfluence logo">
+        <ul class="menu">
+            <li class="menu__menuBtn">
+                <a href="index.php" class="menu__menuBtn--menuLink">Home</a>
+            </li>
+            <li class="menu__menuBtn">
+                <a href="about.php" class="menu__menuBtn--menuLink">About us</a>
+            </li>
+            <li class="menu__menuBtn">
+                <a href="business.php" class="menu__menuBtn--menuLink">Business</a>
+            </li>
+            <li class="menu__menuBtn">
+                <a href="influencer.php" class="menu__menuBtn--menuLink">Influencer</a>
+            </li>
+        </ul>
+        <nav class="rightMenu">
+            <ul>
+                <li>
+                    <i class="fas fa-building"></i> <a href="business.php">Business</a><br/>
+                </li>
+                <li>
+                    <i class="fas fa-user"></i> <a href="influencer.php">Influencer</a>
+                </li>
+            </ul>
+        </nav>   
+</header>
--- index.php
+++ index.php
@@ -14,7 +14,9 @@
 	<!-- Viewport -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<!-- Css, Sass and fonts-->
+	<!-- Css and fonts-->
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+    <link href="css/fontawesome-all.min.css" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 
     <!-- Favicons -->
@@ -24,16 +26,47 @@
 <body>
 
     <div class="gridContainer">
-        <div class="item1">Header</div>
+        <?php include'include/header.php'?>
         <section class="intro">
-            <article class="intro__welcome"><h1 class="intro__welcome--welcomeTxt">We connect brands and influencers</h1></article>
-            <article class="intro__welcomeImg">Intro billede her</article>
+            <article class="intro__welcome">
+                <h1 class="intro__welcome--welcomeTxt">We connect brands and influencers</h1>
+                <p class="intro__welcome--welcomeParagraph">Lorem ipsum dolor sit amet, duo simul audire an. Est cu sapientem sadipscing, duis legimus eum. Me it ipsum sit amet, duo simul audire an. </p>
+                <a href="#"><button><i class="fas fa-user"></i> I am an influencer</button></a>
+                <a href="#"><button><i class="fas fa-building"></i> I am a business</button></a>
+            </article>
+            <article class="intro__welcomeImg">
+                <img src="images/iphone-instagram.png" class="iphoneInstagram" alt="Influencer marketing on Instagram">
+                <img src="images/likes-and-hearts.png" class="likesAndHearts animated zoomIn" alt="Likes and hearts - social media reactions">
+            </article>
         </section>
         <section class="videoContainer">
-            <div class="videoContainer__video"></div>
+            <h2 class="videoContainer__videoHeading">Our video</h2>
+            <iframe src="https://www.youtube.com/embed/WbsZnsr0lI4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen class="videoContainer__video"></iframe>
         </section>
         <section class="keyFeaturesContainer">
-            <div class="keyFeaturesContainer__keyFeatures"></div>
+            <article class="keyFeaturesContainer__keyFeaturesLeft">
+                <h3>Heading</h3>
+                <p>Lorem ipsum dolor sit amet, duo simul audire an. Est cu sapientem sadipscing, duis legimus eum. Me it ipsum sit amet, duo simul audire an.</p>
+                <h3>Heading</h3>
+                <p>Lorem ipsum dolor sit amet, duo simul audire an. Est cu sapientem sadipscing, duis legimus eum. Me it ipsum sit amet, duo simul audire an.</p>
+                <h3>Heading</h3>
+                <p>Lorem ipsum dolor sit amet, duo simul audire an. Est cu sapientem sadipscing, duis legimus eum. Me it ipsum sit amet, duo simul audire an.</p>
+            </article>
+            <article class="keyFeaturesContainer__keyFeatures">
+                <h2 class="keyFeaturesContainer__keyFeaturesHeading">Key features</h2>
+                <img src="images/app-assignments.png"><br/>
+                <a href="http://www.appstore.com" target="_blank"><img src="images/app-store.png"></a>
+                <a href="http://play.google.com" target="_blank"><img src="images/google-play.png"></a>
+                
+            </article>
+            <article class="keyFeaturesContainer__keyFeaturesRight">
+                <h3>Heading</h3>
+                <p>Lorem ipsum dolor sit amet, duo simul audire an. Est cu sapientem sadipscing, duis legimus eum. Me it ipsum sit amet, duo simul audire an.</p>
+                <h3>Heading</h3>
+                <p>Lorem ipsum dolor sit amet, duo simul audire an. Est cu sapientem sadipscing, duis legimus eum. Me it ipsum sit amet, duo simul audire an.</p>
+                <h3>Heading</h3>
+                <p>Lorem ipsum dolor sit amet, duo simul audire an. Est cu sapientem sadipscing, duis legimus eum. Me it ipsum sit amet, duo simul audire an.</p>
+            </article>
         </section>
         <footer class="item3">footer</footer>
     </div>
