d9d262d14650e2355ecadbe8a5c7ebb3dea354f4
LukasBrondel <lukas_broendel@hotmail.com>
2018-05-28 11:04:12 +0200

    Alexander's influcencer.php
    + small fix for business.php

---
+++ influencer.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Title -->
+	<title>Title here</title>
+	
+	<!-- Description -->
+	<meta charset="UTF-8">
+	<meta name="Description" content="Insert your description here">
+
+	<!-- Keywords -->
+	<meta name="keywords" content="HTML,CSS,JavaScript">
+
+	<!-- Viewport -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Css and fonts-->
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+    <link href="css/fontawesome-all.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <!-- Favicons -->
+	<link rel="icon" type="image/png" href="#" sizes="32x32" />
+	<link rel="icon" type="image/png" href="#" sizes="16x16" />
+</head>
+<body>
+
+<div class="gridContainer">
+	<?php include'include/header.php'?>
+
+	<section class="introInfluencer">
+		<article class="introInfluencer__startImg">
+			<img class="mobileBlog" src="images/app-notification-example.png" alt="Influencer notification">
+			<img class="appstoreImg" src="images/app-store.png" alt="App store download">
+			<img class="googleplayImg" src="images/google-play.png" alt="Google Play download">
+		</article>
+
+		<article class="introInfluencer__startHeading">
+			<h1 class="introInfluencer__startHeading--headingTxt">Influencer Heading Test</h1>
+			<p class="introInfluencer__startHeading--paragraphTxt">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ut erat eu elit viverra pellentesque. Nulla nec sem at diam bibendum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;<br><br> Nulla a tincidunt nisi. Nunc fringilla fringilla viverra. Mauris auctor diam vel lorem egestas, nec lobortis lectus vehicula. Mauris eu volutpat dolor. Donec maximus dictum purus vel dignissim. Phasellus ut accumsan tellus.<br><br> Suspendisse rhoncus iaculis arcu, et vulputate leo ornare non. Mauris eget pharetra felis, eu tincidunt nulla</p>
+		</article>
+	</section>
+
+	<section class="stepGuide">
+		<article class="stepGuide__left">
+			<h3 class="stepGuide__left--signUpTxt">1. SIGN UP</h3>
+			<p class="stepGuide__left--paragraphTxt">Nunc fringilla fringilla viverra. Mauris auctor diam vel lorem egestas, nec lobortis lectus vehicula. <br><br> Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae</p>
+		</article>
+
+		<article class="stepGuide__middle">
+			<h2 class="stepGuide__middle--howItWorksTxt">How it works</h2>
+			<h3 class="stepGuide__middle--promoteTxt">2. PROMOTE</h3>
+			<p class="stepGuide__middle--paragraphTxt">Nunc fringilla fringilla viverra. Mauris auctor diam vel lorem egestas, nec lobortis lectus vehicula. <br><br> Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae</p>
+		</article>
+
+		<article class="stepGuide__right">
+			<h3 class="stepGuide__right--getPaidTxt">3. GET PAID</h3>
+			<p class="stepGuide__right--paragraphTxt">Nunc fringilla fringilla viverra. Mauris auctor diam vel lorem egestas, nec lobortis lectus vehicula. <br><br> Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae</p>
+		</article>
+	</section>
+
+	<section class="testimonials">
+		<article class="testimonials__left">
+			<h2 class="testimonials__left--heading">What do our influencers say?</h2>
+			<img class="testimonials__left--pictures" src="images/influencer-man.jpg" alt="Influencer man">
+			<p class="testimonials__left--paragraphTxt"><strong>"</strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ut erat eu elit viverra pellentesque. Nulla nec sem at diam bibendum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.<strong>"</strong></p>
+			<p class="testimonials__left--follow">John Doe, <strong>@johnDoe</strong><br><strong>+30.000</strong> followers on Instagram</p>
+		</article>
+
+		<article class="testimonials__right">
+			<img class="testimonials__right--picturesTest" src="images/influencer-woman.jpg" alt="Influencer man">
+			<p class="testimonials__right--paragraphTxtTest"><strong>"</strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ut erat eu elit viverra pellentesque. Nulla nec sem at diam bibendum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.<strong>"</strong></p>
+			<p class="testimonials__right--follow">John Doe, <strong>@johnDoe</strong><br><strong>+30.000</strong> followers on Instagram</p>
+		</article>
+
+		<article class="testimonials__left">
+			<img class="testimonials__left--pictures" src="images/influencer-woman.jpg" alt="Influencer man">
+			<p class="testimonials__left--paragraphTxt"><strong>"</strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ut erat eu elit viverra pellentesque. Nulla nec sem at diam bibendum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.<strong>"</strong></p>
+			<p class="testimonials__left--follow">John Doe, <strong>@johnDoe</strong><br><strong>+30.000</strong> followers on Instagram</p>
+		</article>
+
+		<article class="testimonials__right">
+			<img class="testimonials__right--pictures" src="images/influencer-man.jpg" alt="Influencer man">
+			<p class="testimonials__right--paragraphTxt"><strong>"</strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ut erat eu elit viverra pellentesque. Nulla nec sem at diam bibendum vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.<strong>"</strong></p>
+			<p class="testimonials__right--follow">John Doe, <strong>@johnDoe</strong><br><strong>+30.000</strong> followers on Instagram</p>
+		</article>
+	</section>
+
+
+	<?php include'include/footer.php'?>
+</div>
+
+<!-- Scripts -->
+<script src="#"></script>
+</body>
+</html>
